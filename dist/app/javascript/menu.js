9de9e48a0880018b4ade9abad741a0f0972ea184
Teepluss <admin@jquerytips.com>
2015-11-16 22:17:46 +0700

    add context menu example

--- dist/app/javascript/menu.js
+++ dist/app/javascript/menu.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var remote = require('remote')
 var Menu = remote.require('menu')
 var MenuItem = remote.require('menu-item')
@@ -23,3 +25,26 @@ window.addEventListener('contextmenu', function (e) {
   e.preventDefault();
   menu.popup(remote.getCurrentWindow());
 }, false);
+
+/*
+// NOTE: the following assumes an ES6 environment
+// The remote module is required to call main process modules
+const remote = require('remote');
+const Menu = remote.require('menu');
+const MenuItem = remote.require('menu-item');
+
+const currentWindow = remote.getCurrentWindow();
+
+let rightClickPosition = null;
+
+const menu = new Menu();
+menu.append(new MenuItem({ label: 'Inspect Element', click: () => {
+  currentWindow.inspectElement(rightClickPosition.x, rightClickPosition.y);
+} }));
+
+window.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+  rightClickPosition = {x: e.x, y: e.y};
+  menu.popup(currentWindow);
+}, false);
+*/
--- dist/app/jsx/components/download/Index.jsx
+++ dist/app/jsx/components/download/Index.jsx
@@ -2,12 +2,37 @@
 
 var React = require('react');
 
+// Context menu.
+var remote = require('remote')
+var Menu = remote.require('menu')
+var MenuItem = remote.require('menu-item')
+
 var Index = React.createClass({
+  onClick: function() {
+    alert('Please do right click!');
+  },
+  onContextMenu: function(e, id) {
+    // Stop global context menu
+    e.preventDefault();
+    e.stopPropagation();
+
+    // Build our new menu
+    var menu = new Menu()
+    menu.append(new MenuItem({
+      label: 'Copy',
+      click: function() {
+        // Trigger an alert when menu item is clicked
+        alert('Copy Item ' + id);
+      }
+    }))
+
+    menu.popup(remote.getCurrentWindow());
+  },
   render: function() {
     return (
       <div className="pane">
         <div className="app-inside">
-          {'Download'}
+          <button onClick={this.onClick} onContextMenu={(e) => this.onContextMenu(e, "Tee")}>{'Download'}</button>
         </div>
       </div>
     )
